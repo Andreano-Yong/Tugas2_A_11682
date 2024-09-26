e665f737cbd90bfe62ea2a97d3811baa3b815bf5
Andreano-Yong <220711682@students.uajy.ac.id>
2024-09-26 17:06:29 +0700

    Tugas_Modul 2_A_Andreano_11682.py

--- Tugas_Modul 2_A_Andreano_11682.py
+++ Tugas_Modul 2_A_Andreano_11682.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = 'C:\Tugas2_A_11682'
-model=os.path.join(model_path, 'GBT_heartDisease_model.pkl')
+model_path = 'GBT_heartDisease_model.pkl'
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
